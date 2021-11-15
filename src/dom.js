27800183f263c562abdc7702f585851fce05620e
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2021-11-15 19:48:26 +0100

    Allow to place ships in valid coordinates

--- src/dom.js
+++ src/dom.js
@@ -60,32 +60,89 @@ class Render {
 
   static menuPlaceShips() {
     const shipsInfo = [
-      { type: 'carrier', length: 5 },
-      { type: 'battleship', length: 4 },
-      { type: 'destroyer', length: 3 },
-      { type: 'submarine', length: 3 },
-      { type: 'patrol', length: 2 },
+      { type: 'carrier', length: 5, align: 'vertical' },
+      { type: 'battleship', length: 4, align: 'vertical' },
+      { type: 'destroyer', length: 3, align: 'vertical' },
+      { type: 'submarine', length: 3, align: 'vertical' },
+      { type: 'patrol', length: 2, align: 'vertical' },
     ];
     const { gameBoard } = gameboardHuman;
     const divGameboard = document.getElementById('copy-gameboard');
+    const buttonChangeAlign = document.getElementById('change-align');
+    buttonChangeAlign.addEventListener('click', () => this.changeAlign(shipsInfo));
     for (let i = 0; i < gameBoard.length; i++) {
       for (let j = 0; j < gameBoard[i].length; j++) {
         const cell = document.createElement('div');
         cell.setAttribute('y-position', i);
         cell.setAttribute('x-position', j);
-
-        cell.addEventListener('mouseover', (e) => this.mouseOverPlacing(e, shipsInfo));
-
         cell.classList.add('cell-computer');
+        cell.addEventListener('mouseover', (e) => this.mouseOverPlacing(e, shipsInfo));
+        cell.addEventListener('mouseleave', (e) => this.mouseLeavePlacing(e, shipsInfo));
 
         divGameboard.appendChild(cell);
       }
     }
   }
-  
-  static mouseOverPlacing(e, shipsInfo){
-    console.log(e)
+
+  static changeAlign(shipsInfo) {
+    if (shipsInfo[0].align === 'vertical') {
+      shipsInfo[0].align = 'horizontal';
+    } else {
+      shipsInfo[0].align = 'vertical';
+    }
+  }
+
+  static mouseOverPlacing(e, shipsInfo) {
+    // Get the gameboard and if its set up to vertical or horizontal, take ship's length and
+    // make a for loop (X or Y)  to get the div and change color
+    console.log(e);
     console.log(shipsInfo);
+    console.log(shipsInfo);
+    const shipLength = shipsInfo[0].length;
+    let xPosition = Number(e.target.getAttribute('x-position'));
+    let yPosition = Number(e.target.getAttribute('y-position'));
+    for (let i = 0; i < shipLength; i++) {
+      console.log(yPosition);
+      const cellHover = document.querySelector(
+        `[x-position="${xPosition}"][y-position="${yPosition}"]`
+      );
+      if (xPosition < 10 && yPosition < 10) {
+        if (shipsInfo[0].align === 'vertical') {
+          yPosition += 1;
+        } else {
+          xPosition += 1;
+        }
+        cellHover.classList.toggle('hit-cell');
+      } else {
+        const divGameboard = document.getElementById('copy-gameboard')
+        divGameboard.classList.add('hit-forbidden');
+        console.log(divGameboard)
+      }
+    }
+  }
+
+  static mouseLeavePlacing(e, shipsInfo) {
+    const shipLength = shipsInfo[0].length;
+    let xPosition = Number(e.target.getAttribute('x-position'));
+    let yPosition = Number(e.target.getAttribute('y-position'));
+    for (let i = 0; i < shipLength; i++) {
+      const cellHover = document.querySelector(
+        `[x-position="${xPosition}"][y-position="${yPosition}"]`
+      );
+      if (cellHover !== null) {
+        if (shipsInfo[0].align === 'vertical') {
+          yPosition += 1;
+        } else {
+          xPosition += 1;
+        }
+        cellHover.classList.toggle('hit-cell');
+      } else {
+        const divGameboard = document.getElementById('copy-gameboard')
+        
+        divGameboard.classList.toggle('hit-forbidden');
+        console.log(divGameboard)
+      }
+    }
   }
 }
 
